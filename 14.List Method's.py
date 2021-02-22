e93c0d32ae4a70ca7dd8e223957e3663265de7ae
Murad753 <74384654+Murad753@users.noreply.github.com>
2021-02-21 18:18:01 -0800

    Python Research

---
+++ 14.List Method's.py
@@ -0,0 +1,93 @@
+adlar = ['Murad', 'Xeyal', 'Samir', 'Nurlan']
+yaslar = [1998, 2000, 1998, 1987]
+
+# 1. Ilahe adini list-in sonuna elave edin.
+
+# adlar.append('Ilahe')
+
+
+
+# 2. Ofelya adini list-in evveline elave edin.
+
+# adlar.insert(0,'Ofelya')
+
+
+
+# 3. Xeyal adini list-den silin.
+
+# adlar.remove('Xeyal')
+
+
+
+# 4. Murad adinin index-i nedir ?
+
+# indexno = adlar.index('Murad')
+
+
+
+# 5. Beril list-in bir deyeridir mi ? 
+
+# if 'Beril' in adlar:
+#     print("Beril List-in bir deyeridir")
+
+
+
+# 6. List deyerlerini ters cevirin.
+
+# adlar.reverse()
+
+
+
+# 7. List deyerlerini elifbaya uygun olaraq siralayin.
+
+# adlar.sort()
+
+
+
+# 8. Years list-ni reqemsal boyukluye gore siralayin.
+
+# yaslar.sort()
+# yaslar.reverse()
+
+
+
+# 9. string = 'IPhone X, IPhone XR' karakter sirasini list-e cevirin
+# string = 'IPhone X, IPhone XR'
+# string.split(',')
+
+
+
+# 10. yaslar list-nin en boyuk ve en kicik deyeri nedir ? 
+
+# minimum = min(yaslar)
+# maximum = max(yaslar)
+
+# print(minimum)
+# print(maximum)
+
+
+
+# 11. Yaslar list-de nece dene 1998 deyeri vardir ?
+
+# print(yaslar.count(1998))
+
+
+
+# 12. Yaslar list-nin butun deyerlerini silin.
+
+# yaslar.clear()
+
+
+
+# 13. Istifadeci terefinden girilen dinamik sayda marka deyerlerini list icinde yerlestirin.
+
+# markalar = []
+
+# ranges = int(input('Nece Marka deyeriniz var : '))
+
+# for x in range(ranges):
+#     marka = input('Marka Deyerini Girin: ')
+#     markalar.append(marka)
+#     print(markalar)
+
+
